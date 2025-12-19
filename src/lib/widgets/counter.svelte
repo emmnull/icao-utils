d4c9ccffcb1430c4518de93fc53a0654e732e45a
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-12-19 14:03:01 -0500

    init

---
+++ src/lib/widgets/counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { value = $bindable() }: { value: number } = $props();
+</script>
+
+<div>
+	<button onclick={() => value--}> - </button>
+	<span>{value}</span>
+	<button onclick={() => value++}> + </button>
+</div>
+
+<style>
+</style>
